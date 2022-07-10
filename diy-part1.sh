4c27692eec5acadcc075b61124d6725861bdfa6a
cyh92 <cyh92@live.cn>
2022-07-10 21:25:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# git clone -b v1.7.2 --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b v1.7.2 --depth=1 https://github.com/cyh92/luci-theme-argon package/luci-theme-argon
-rm -rf ./feeds/luci/themes/luci-themes-argon
+# git clone -b v1.7.2 --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+find ./ -name luci-theme-argon | xargs rm -rf;
+#拉取 argon的源码
+git clone --depth=1 https://github.com/cyh92/luci-theme-argon package/lean/luci-theme-argon
